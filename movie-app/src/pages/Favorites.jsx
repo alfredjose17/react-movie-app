6f8dc6c9a81ee3bce861c218207efdebd64f73f8
Alfred Varghese Jose <alfredvjose17@gmail.com>
2026-03-11 23:08:07 -0400

    Adding basic app setup

---
+++ movie-app/src/pages/Favorites.jsx
@@ -0,0 +1,11 @@
+import '../css/Favorites.css'
+
+function Favorites() {
+    return (
+        <div className="favorites-empty">
+            <h2>No Favorite Movies Yet</h2>
+        </div>
+    )
+}
+
+export default Favorites
